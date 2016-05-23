852b24d4e52eb8db9aafd653b3a0258d8238cb98
Sinduja Rangarajan <cynduja@gmail.com>
2016-05-22 21:53:32 -0700

    Added other_info field to services.html

--- oneliner/models.py
+++ oneliner/models.py
@@ -30,7 +30,7 @@ class Service(models.Model):
     performance = models.CharField(max_length=3000, blank=True, null=True)
     privacy = models.CharField(max_length=3000, blank=True, null=True)
     account_id_label = models.CharField(max_length=250, blank=True, null=True)
-    Other_information = models.CharField(max_length=1000, blank=True, null=True)
+    other_information = models.CharField(max_length=1000, blank=True, null=True)
 
     def __str__(self):
         return self.name
